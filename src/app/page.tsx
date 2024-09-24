a9b9f8ddd88bacd904726debca658a9a7e0fb002
Halyna Savchenko <galasavchenko1@gmail.com>
2024-09-24 22:07:16 +0200

    client component vs server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import styles from "./page.module.css";
 import course from './course.json';
 import { Lesson } from "./lesson";
+import dynamic from 'next/dynamic';
+
+const ClientComponent = dynamic(() => import('./some-component'), {ssr: false})
 
 async function getAllCourses(): Promise<typeof course> { return new Promise((resolve) => {
   setTimeout(() => resolve(course), 1000)
@@ -12,6 +15,7 @@ export default async function Home() {
   const courses = await getAllCourses();
   return (
       <main className={styles.main}>
+        <ClientComponent/>
           <ul>
             {courses.lessons.map((lesson) => {
               return <li key={lesson.name}>
---
+++ src/app/some-component.tsx
@@ -0,0 +1,8 @@
+'use client'
+
+export default function SomeComponent() {
+    console.log('SOME COMPONENT RENDERED');
+    return (
+        <p>SOME SUPER COMPONENT IS HERE</p>
+    )
+}
